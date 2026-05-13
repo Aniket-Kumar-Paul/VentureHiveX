dd5fbf642383f65d580fc363aa5674afaf8e087e
Aniket Kumar Paul <aniketkumarpaul1@gmail.com>
2026-05-13 19:53:24 +0530

    NEXT Config Fixes

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,15 +1,12 @@
 /** @type {import('next').NextConfig} */
-
-// const nextConfig = {
-// };
-// UPDATE THE BELOW AFTER DEV TESTING
 const nextConfig = {
-  allowedDevOrigins: ['0baf-2401-4900-1c5c-696c-d1dc-5ee5-4d0c-cfeb.ngrok-free.app'],
   async rewrites() {
+    const backendUrl = process.env.API_BACKEND_URL || 'http://localhost:8000';
+    
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8000/api/:path*',
+        destination: `${backendUrl}/api/:path*`,
       },
     ];
   },
